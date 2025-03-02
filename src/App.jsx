0a1887f132767c4ed7e908e6534c6c4abd6d95b3
Rafael Niebles <rnieblesealo@gmail.com>
2025-03-02 11:06:38 -0500

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import cardInfo from "./cardInfo"
+import { RiDiceLine } from "react-icons/ri";
+
 export default function App() {
   const ControlButton = (props) => (
-    <div className="bg-black w-[100px] text-center p-3 rounded-2xl">
-      {props.text}
-    </div>
+    <button className="flex items-center gap-2 bg-black w-min-content text-center p-3 pl-6 pr-6 rounded-2xl transition-transform duration-short transform hover:scale-110 transform active:scale-100">
+      <span className="font-bold">
+        {props.text}
+      </span>
+      <span className="text-2xl">
+        {props.graphic}
+      </span>
+    </button>
   )
 
-  const Card = () => (
+  const Card = (props) => (
     /*
      * Hoverable area
      *  Flipping area
@@ -13,16 +21,23 @@ export default function App() {
      *    Back face (rotated 180 on y)
     */
 
-    <div className="perspective-[1000px] cursor-pointer drop-shadow-2xl">
-      <div className="w-[450px] aspect-square relative transform-3d transition-transform duration-300 transform hover:rotate-x-180">
+    <div className="perspective-distant cursor-pointer z-20">
+      <div className="w-lg aspect-square relative transform-3d transition-transform duration-300 transform hover:rotate-x-180">
         <div className="rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-0">
           <img
-            src="nobuses.jpg"
+            src={props.imgSrc}
             className="w-full aspect-square"
           />
         </div>
-        <div className="text-center flex items-center text-[2.5rem] font-bold justify-center bg-gray-300 rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-180">
-          "No Buses"
+        <div className="text-center flex items-center text-3xl font-bold justify-center bg-gray-300 rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-180">
+          <div className="flex flex-col">
+            <span className="text-5xl">
+              {props.albumName ?? "..."}
+            </span>
+            <span className="text-2xl font-normal text-gray-500">
+              by {props.artistName ?? "..."}
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -30,11 +45,20 @@ export default function App() {
 
   return (
     <div className="flex items-center justify-center flex-col gap-3">
-      <p className="font-bold text-[2rem]">What's this album?</p>
-      <Card />
-      <div className="flex items-center justify-center flex-row rounded-2xl gap-5 font-bold text-white">
-        <ControlButton text="Prev." />
-        <ControlButton text="Next." />
+      <h1 className="text-4xl font-bold">Guess the Artist!</h1>
+      <div className="text-center text-gray-500">
+        <p className="flex items-center justify-center gap-2 text-md">Let's see if you can guess the artist of these
+          <span className="bg-gray-300 rounded-lg p-1 pl-2 pr-2 font-bold"> 10 </span>
+          albums!
+        </p>
+      </div>
+      <Card
+        imgSrc={cardInfo[0].imgSrc}
+        albumName={cardInfo[0].albumName}
+        artistName={cardInfo[0].artistName}
+      />
+      <div className="bg-transparent text-white flex items-center justify-center flex-row rounded-2xl gap-5">
+        <ControlButton text="Another!" graphic={<RiDiceLine />} />
       </div>
     </div>
   )
---
+++ src/cardInfo.js
@@ -0,0 +1,54 @@
+const cardInfo = [
+  {
+    imgSrc: "7th-street.jpg",
+    artistName: "Fake Tides",
+    albumName: "7th Street"
+  },
+  {
+    imgSrc: "boring-thing.jpg",
+    artistName: "No Buses",
+    albumName: "Boring Thing EP"
+  },
+  {
+    imgSrc: "bowl-cut.jpg",
+    artistName: "Jaded Juice Riders",
+    albumName: "Bowl Cut"
+  },
+  {
+    imgSrc: "buds.jpg",
+    artistName: "Surf Curse",
+    albumName: "Buds"
+  },
+  {
+    imgSrc: "carpet-dope.jpg",
+    artistName: "The Pesos",
+    albumName: "Carpet Dope"
+  },
+  {
+    imgSrc: "hum.jpg",
+    artistName: "Hum",
+    albumName: "You'd Prefer an Astronaut"
+  },
+  {
+    imgSrc: "meta-y-dinero.jpg",
+    artistName: "Los Blenders",
+    albumName: "Meta y Dinero"
+  },
+  {
+    imgSrc: "meth-wax.jpg",
+    artistName: "Meth Wax",
+    albumName: "Meth Wax"
+  },
+  {
+    imgSrc: "mourning-star.jpg",
+    artistName: "Rehash",
+    albumName: "Mourning Star"
+  },
+  {
+    imgSrc: "o-tortuga.jpg",
+    artistName: "O Tortuga",
+    albumName: "O Tortuga"
+  }
+]
+
+export default cardInfo;
