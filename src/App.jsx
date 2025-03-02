6e896dd813521b7ddce5bc51dc7828a13d4c78bc
Rafael Niebles <rnieblesealo@gmail.com>
2025-03-02 12:41:57 -0500

    final ver for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import cardInfo from "./cardInfo"
+import clsx from "clsx"
+import { useState, useEffect } from "react"
 import { RiDiceLine } from "react-icons/ri";
 
 export default function App() {
   const ControlButton = (props) => (
-    <button className="flex items-center gap-2 bg-black w-min-content text-center p-3 pl-6 pr-6 rounded-2xl transition-transform duration-short transform hover:scale-110 transform active:scale-100">
+    <button onClick={props.onClick} className="cursor-pointer flex items-center gap-2 bg-gray-900 w-min-content text-center p-3 pl-6 pr-6 rounded-2xl transition-transform duration-short transform hover:scale-110 transform active:scale-100 z-20">
       <span className="font-bold">
         {props.text}
       </span>
@@ -13,7 +15,7 @@ export default function App() {
     </button>
   )
 
-  const Card = (props) => (
+  const Card = (props) => {
     /*
      * Hoverable area
      *  Flipping area
@@ -21,44 +23,72 @@ export default function App() {
      *    Back face (rotated 180 on y)
     */
 
-    <div className="perspective-distant cursor-pointer z-20">
-      <div className="w-lg aspect-square relative transform-3d transition-transform duration-300 transform hover:rotate-x-180">
-        <div className="rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-0">
-          <img
-            src={props.imgSrc}
-            className="w-full aspect-square"
-          />
-        </div>
-        <div className="text-center flex items-center text-3xl font-bold justify-center bg-gray-300 rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-180">
-          <div className="flex flex-col">
-            <span className="text-5xl">
-              {props.albumName ?? "..."}
-            </span>
-            <span className="text-2xl font-normal text-gray-500">
-              by {props.artistName ?? "..."}
-            </span>
+    const [flipped, setFlipped] = useState(false)
+
+    const flip = clsx(
+      flipped ? "transform rotate-x-180" : "transform rotate-x-0"
+    )
+
+    return (
+      <button onClick={() => { setFlipped(!flipped) }} className="animate-fade-up animate-ease-out animate-duration-500 perspective-distant cursor-pointer z-20">
+        <div className={`w-lg aspect-square relative transform-3d transition-transform duration-300 ${flip}`}>
+          <div className="rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-0">
+            <img
+              src={props.imgSrc}
+              className="w-full aspect-square"
+            />
+          </div>
+          <div className="text-center text-white flex items-center text-3xl font-bold justify-center bg-gray-900 rounded-2xl overflow-hidden backface-hidden absolute w-full h-full rotate-x-180">
+            <div className="flex flex-col">
+              <span className="text-5xl">
+                {`"${props.albumName}"`}
+              </span>
+              <span className="text-2xl font-normal text-gray-500">
+                by {props.artistName}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  )
+      </button>
+    )
+  }
+
+  const [imgSrc, setImgSrc] = useState(null)
+  const [albumName, setAlbumName] = useState(null)
+  const [artistName, setArtistName] = useState(null)
+
+  function newCard() {
+    const randomCard = cardInfo[Math.floor(Math.random() * cardInfo.length)]
+
+    setImgSrc(randomCard.imgSrc)
+    setAlbumName(randomCard.albumName)
+    setArtistName(randomCard.artistName)
+
+    // set accent color in css!
+    document.documentElement.style.setProperty("--accent-color", randomCard.color)
+  }
+
+  // get new card on component mount
+  useEffect(() => {
+    newCard()
+  }, [])
 
   return (
-    <div className="flex items-center justify-center flex-col gap-3">
-      <h1 className="text-4xl font-bold">Guess the Artist!</h1>
-      <div className="text-center text-gray-500">
-        <p className="flex items-center justify-center gap-2 text-md">Let's see if you can guess the artist of these
-          <span className="bg-gray-300 rounded-lg p-1 pl-2 pr-2 font-bold"> 10 </span>
-          albums!
+    <div className="w-screen h-screen flex items-center justify-center flex-col gap-3">
+      <h1 className="text-white text-4xl font-bold">Guess the Artist!</h1>
+      <div className="text-center">
+        <p className="flex items-center justify-center gap-2 text-md text-white">Can you guess the artist of these
+          <span className="outline rounded-lg p-1 pl-2 pr-2 font-bold text-white"> {cardInfo.length} </span>
+          albums?
         </p>
       </div>
       <Card
-        imgSrc={cardInfo[0].imgSrc}
-        albumName={cardInfo[0].albumName}
-        artistName={cardInfo[0].artistName}
+        imgSrc={imgSrc}
+        albumName={albumName}
+        artistName={artistName}
       />
       <div className="bg-transparent text-white flex items-center justify-center flex-row rounded-2xl gap-5">
-        <ControlButton text="Another!" graphic={<RiDiceLine />} />
+        <ControlButton text="Another!" graphic={<RiDiceLine />} onClick={newCard} />
       </div>
     </div>
   )
--- src/cardInfo.js
+++ src/cardInfo.js
@@ -2,52 +2,62 @@ const cardInfo = [
   {
     imgSrc: "7th-street.jpg",
     artistName: "Fake Tides",
-    albumName: "7th Street"
+    albumName: "7th Street",
+    color: "#403224"
   },
   {
     imgSrc: "boring-thing.jpg",
     artistName: "No Buses",
-    albumName: "Boring Thing EP"
+    albumName: "Boring Thing EP",
+    color: "#42889D"
   },
   {
     imgSrc: "bowl-cut.jpg",
     artistName: "Jaded Juice Riders",
-    albumName: "Bowl Cut"
+    albumName: "Bowl Cut",
+    color: "#2B5C5F"
   },
   {
     imgSrc: "buds.jpg",
     artistName: "Surf Curse",
-    albumName: "Buds"
+    albumName: "Buds",
+    color: "#8A8B9F"
   },
   {
     imgSrc: "carpet-dope.jpg",
     artistName: "The Pesos",
-    albumName: "Carpet Dope"
+    albumName: "Carpet Dope",
+    color: "#A59086"
   },
   {
     imgSrc: "hum.jpg",
     artistName: "Hum",
-    albumName: "You'd Prefer an Astronaut"
+    albumName: "You'd Prefer an Astronaut",
+    color: "#8EFA8A"
   },
   {
     imgSrc: "meta-y-dinero.jpg",
     artistName: "Los Blenders",
-    albumName: "Meta y Dinero"
+    albumName: "Meta y Dinero",
+    color: "#DA389D"
   },
   {
     imgSrc: "meth-wax.jpg",
     artistName: "Meth Wax",
-    albumName: "Meth Wax"
+    albumName: "Meth Wax",
+    color: "#EC8FF5"
   },
   {
     imgSrc: "mourning-star.jpg",
     artistName: "Rehash",
-    albumName: "Mourning Star"
+    albumName: "Mourning Star",
+    color: "#E6C273"
   },
   {
     imgSrc: "o-tortuga.jpg",
     artistName: "O Tortuga",
-    albumName: "O Tortuga"
+    albumName: "O Tortuga",
+    color: "#3775A7"
   }
 ]
 
